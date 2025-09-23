6d7ac0e66e04ddf6ccfa8989b16590d311757067
slava22801 <aldarlamazhyk@yandex.ru>
2025-09-23 09:59:58 +0700

    add route

--- tg_client/main.py
+++ tg_client/main.py
@@ -1,1 +1,33 @@
-import aiogram
+import asyncio
+import logging
+import sys
+from os import getenv
+
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from routes import route
+
+BOT_TOKEN = ""
+
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def bot_start(message: Message) -> None:
+    await message.answer("Hello")
+
+async def main() -> None:
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp.include_router(router=route)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
